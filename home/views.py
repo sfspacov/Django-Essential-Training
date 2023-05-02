3e1f8f39a5e99fb2216a897eaa1af193db139a5c
sfspacov <stephan_fs@hotmail.com>
2023-05-02 12:32:17 -0500

    class-based

--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
 from datetime import date
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': date.today().strftime('%d/%m/%Y')})
+class HomeView(TemplateView):
+    template_name='home/welcome.html'
+    extra_context = {'today': date.today()}
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
