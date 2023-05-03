77d7282e5d90092c4e50e8f59f4413d1550167a0
sfspacov <stephan_fs@hotmail.com>
2023-05-03 12:01:09 -0500

    Finish crud operations

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import NotesForm
 from .models import Notes
 
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'    
+    template_name = 'notes/notes_delete.html'
+    
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+    
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
