a03e3c4aa1a9f5c87cf595e497417147a1303f8f
sfspacov <stephan_fs@hotmail.com>
2023-05-08 16:31:05 -0500

    login/logout

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from datetime import date
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+class LoginInterfaceView(LoginView):
+    template_name = "home/login.html"
 
 class HomeView(TemplateView):
     template_name='home/welcome.html'
